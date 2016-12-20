fa1a4e883952f64b1198accdfe7fe16ee9833d99
littlemonstersites <dave@lmsites.co.uk>
2016-12-20 17:39:29 +0000

    Fixed DAL issue
    Hopefully fixed DAL issue

--- Stage-plan.Bll/SubscriptionPreferences.cs
+++ Stage-plan.Bll/SubscriptionPreferences.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,88 @@ namespace Stage_plan.Bll
 {
     public class SubscriptionPreferences
     {
+        private Dal.StageplanEntities _dc;
+        private string _faultSavingMessage = "There was a fault. We could not save the change. Please email us direct";
+
+        public SubscriptionPreferences()
+        {
+            this._dc = new Dal.StageplanEntities();
+        }
+
+        [Required]
+        [Display(Name="Name")]
         public string Name { get; set; }
+
+
+        [Required]
+        [Display(Name = "Email address")]
         public string EmailAddress { get; set; }
-        public string OptInPreference { get; set; }
+
+
+        [Required]
         public bool IsOptIn { get; set; }
+
+        public string Update()
+        {
+            string result = String.Empty;
+            if (IsOptIn)
+                result = OptIn();
+            else
+                result = OptOut();
+
+            return result;
+        }
+
+        public Dal.MailingList GetEmail()
+        {
+            var email = ((from d in this._dc.MailingLists
+                          where d.EmailAddress == this.EmailAddress
+                          select d).SingleOrDefault());
+            return email;
+        }
+
+        private string OptIn()
+        {
+            var email = GetEmail();
+            if (email != null && email.IsOptin)
+                return "The email address provided is already opt in.";
+
+            email = new Dal.MailingList()
+            {
+                ConfirmToken = Guid.NewGuid().ToString(),
+                DateOptInRequest = DateTime.Now,
+                EmailAddress = this.EmailAddress,
+                IsOptin = this.IsOptIn,
+                Name = this.Name
+            };
+
+            this._dc.MailingLists.Add(email);
+            return Save() ? String.Empty : this._faultSavingMessage;
+        }
+
+        private string OptOut()
+        {
+            var email = GetEmail();
+
+            if (email == null)
+                return "Sorry, we don't have any match for the email address provided.";
+
+            this._dc.MailingLists.Remove(email);
+            return Save() ? String.Empty : this._faultSavingMessage;
+        }
+
+        private bool Save()
+        {
+            try
+            {
+                this._dc.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                //TODO send fail email
+                return false;
+            }
+        }
     }
 }
---
+++ Stage-plan.Tests/Subscription.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stage_plan.Bll;
+
+namespace Stage_plan.Tests
+{
+    [TestClass]
+    public class Subscription
+    {
+        [TestMethod]
+        public void OptIn()
+        {
+            var email = GetEmail(true);
+            OptIn(email);
+            Delete(email);//clean up
+        }
+       
+        private void OptIn(SubscriptionPreferences email)
+        {
+            var result = email.Update();
+
+            Assert.IsTrue(String.IsNullOrEmpty(result));
+
+            Assert.IsFalse(email.GetEmail() == null);
+        }
+
+        [TestMethod]
+        public void OptOut()
+        {
+            var email = GetEmail(true);
+            OptIn(email);
+
+            email = GetEmail(false);
+
+            var result = email.Update();
+
+            Assert.IsTrue(String.IsNullOrEmpty(result));
+            Delete(email);//clean up
+        }
+
+        [TestMethod]
+        public void OptInFromOptOut()
+        {
+        }
+
+        [TestMethod]
+        public void OptOutWithUnrecognisedEmailAddress()
+        {
+        }
+
+        [TestMethod]
+        public void OptOutWithIncompleteModel()
+        {
+        }
+
+        private Bll.SubscriptionPreferences GetEmail(bool isOptIn)
+        {
+            return new Bll.SubscriptionPreferences()
+            {
+                EmailAddress = "dave@stage-plan-test.co.uk",
+                Name = "My test name",
+                IsOptIn = isOptIn
+            };
+        }
+
+        private bool Delete(Bll.SubscriptionPreferences email)
+        {
+
+            Dal.StageplanEntities dc = new Dal.StageplanEntities();
+            var ml = dc.MailingLists.Where(a => a.EmailAddress == email.EmailAddress).SingleOrDefault();
+
+            if (ml != null)
+            {
+                try
+                {
+                    dc.MailingLists.Remove(ml);
+                    dc.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw new Exception();
+                }
+            }
+            return false;
+        }
+    }
+}
